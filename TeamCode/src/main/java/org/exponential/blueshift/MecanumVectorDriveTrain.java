7652d68fa4ad65b75730071149be2768392c6fe0
baconsuperguy <keertikbacon@yahoo.com>
2019-09-17 16:12:39 -0400

    Began to implement vector movement system for mecanum wheels

---
+++ TeamCode/src/main/java/org/exponential/blueshift/MecanumVectorDriveTrain.java
@@ -0,0 +1,23 @@
+package org.exponential.blueshift;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.exponential.superclasses.Drivetrain;
+
+public class MecanumVectorDriveTrain extends Drivetrain {
+
+
+    @Override
+    public float encoderToInch(int encoder) {
+        return 0;
+    }
+
+    @Override
+    public int inchToEncoder(float inches) {
+        return 0;
+    }
+
+    @Override
+    public void initialize(OpMode opMode) {
+
+    }
+}
