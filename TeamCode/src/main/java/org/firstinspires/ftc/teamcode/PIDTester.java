d33a322cc50752336180028d493a80e2591c7f39
Ere <eresoh1@gmail.com>
2019-10-12 13:14:45 -0400

    implemented 3 trials for each p value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTester.java
@@ -16,6 +16,9 @@ import java.util.ArrayList;
 @Autonomous(name="PID Tester", group="TeleOp")
 
 public class PIDTester extends LinearOpMode {
+
+    int trials =3;
+
     @Override
     public void runOpMode() throws InterruptedException {
         PathComponent[] pathComponents=
@@ -36,10 +39,11 @@ public class PIDTester extends LinearOpMode {
         float currentValue = 0;
         boolean showResults = false;
         ArrayList<Double> times = new ArrayList<Double>();
+        int leastIndex = 0;
 
         while(opModeIsActive()){
 
-            //dpad for small increments
+            //dpad for big increments
             if(gamepad1.dpad_up){
                 currentValue+= bigIncrement;
                 telemetry.update();
@@ -50,7 +54,7 @@ public class PIDTester extends LinearOpMode {
                 telemetry.update();
                 while (gamepad1.dpad_down && opModeIsActive());
             }
-            //joystick for big jumps
+            //bumpers for small increments
             if(gamepad1.right_bumper){
                 currentValue+= smallIncrement;
                 telemetry.update();
@@ -67,35 +71,41 @@ public class PIDTester extends LinearOpMode {
                 telemetry.update();
                 while(gamepad1.b && opModeIsActive());
             }
-            //a to start or continue
+            //a to start testing
             if(gamepad1.a){
                 //start testing
                 showResults = true;
                 ElapsedTime clock = new ElapsedTime();
+                double[] alltrials = new double[trials];
                 for(float value: values){
-                    telemetry.addData("Current P", value);
-                    telemetry.update();
-                    clock.reset();
-                    ExampleBot.drivetrain.resetOrientation();
-                    ExampleBot.drivetrain.resetAllEncoders();
-                    ExampleBot.drivetrain.setAllRunUsingEncoders();
-                    PIDController testPID = new PIDController(value, 0, 0);
-                    testPID.start();
-                    while (!ExampleBot.drivetrain.movePath(path, testPID) && opModeIsActive()){
-                        telemetry.addData("Current Time",clock.seconds());
+                    for(int i = 0; i < trials; i++) {
+                        telemetry.addData("Current P", value);
                         telemetry.update();
-                    }
+                        clock.reset();
+                        resetRobot();
+                        PIDController testPID = new PIDController(value, 0, 0);
+                        testPID.start();
+                        while (!ExampleBot.drivetrain.movePath(path, testPID) && opModeIsActive()) {
+                            telemetry.addData("Current Time", clock.seconds());
+                            telemetry.update();
+                        }
 //                    ExampleBot.drivetrain
-                    Double timeToAdd = clock.seconds();
-                    telemetry.addData("Total Time", timeToAdd);
-                    telemetry.update();
-                    times.add(timeToAdd);
+                        Double timeToAdd = clock.seconds();
+                        telemetry.addData("Total Time", timeToAdd);
+                        telemetry.update();
+                        alltrials[i] = timeToAdd;
+                        while (!gamepad1.x && opModeIsActive()) ;
+                    }
+                    times.add(calculateAverage(alltrials));
                     while(!gamepad1.x && opModeIsActive());
                 }
+                leastIndex = findLeast(times);
             }
             if(showResults){
                 telemetry.addData("P Values", values);
                 telemetry.addData("Times", times);
+                telemetry.addData("Best P Value", values.get(leastIndex));
+                telemetry.addData("Best P Value Time", times.get(leastIndex));
             }
             else {
                 telemetry.addData("current P value", currentValue);
@@ -104,55 +114,37 @@ public class PIDTester extends LinearOpMode {
             telemetry.update();
         }
 
-        //ExampleBot.horizontalSlides.move(500);
-        //move horizontal slides
-        //while(true&&opModeIsActive()){
-        //  ExampleBot.drivetrain.moveLeftTest();
-        //}
-/*
-        while (!ExampleBot.drivetrain.movePath(path)) { //moves the robot along the path, while loop ends when path is complete (move method returns false)
-            //ExampleBot.verticalSlides.move(200); //moves the vertical slides while the robot is following the path
-        }
-
-        while (ExampleBot.drivetrain.pivot(90f, .5f)); //turns the robot another 90 degrees
- */
-
     }
 
-    private double findLeast(ArrayList<Double> a){
+    private void resetRobot(){
+        ExampleBot.drivetrain.resetOrientation();
+        ExampleBot.drivetrain.resetAllEncoders();
+        ExampleBot.drivetrain.setAllRunUsingEncoders();
+    }
+
+    //given list of trials for one p value
+    //return average
+    private double calculateAverage(double[] array){
+        double sum = 0;
+        for(double d: array){
+            sum+=d;
+        }
+
+        return sum/((double) array.length);
+    }
+
+    //given list of times
+    //return index of least time
+    private int findLeast(ArrayList<Double> a){
         if(a.size() == 1)
-            return a.get(0);
-        double least = a.get(0);
+            return 0;
+        int least = 0;
         for(int i = 1; i < a.size(); i++){
             if(a.get(i) < least){
-                least = a.get(i);
+                least = i;
             }
         }
 
         return least;
     }
-
-//    private void startTester(ArrayList<Float> values, Path path){
-//        ElapsedTime clock = new ElapsedTime();
-//        ArrayList<Double> times = new ArrayList<Double>();
-//        for(float value: values){
-//            telemetry.addData("Current P", value);
-//            telemetry.update();
-//            clock.reset();
-//            ExampleBot.drivetrain.resetAllEncoders();
-//            ExampleBot.drivetrain.setAllRunUsingEncoders();
-//            PIDController testPID = new PIDController(value, 0, 0);
-//            testPID.start();
-//            while (!ExampleBot.drivetrain.movePath(path, testPID) && opModeIsActive()){
-//                telemetry.addData("Current Time",clock.seconds());
-//                telemetry.update();
-//            }
-//            times.add(clock.seconds());
-//            while(!gamepad1.x && opModeIsActive());
-//        }
-//
-//        telemetry.addData("P Values", values);
-//        telemetry.addData("Times", times);
-//        telemetry.update();
-//    }
 }
